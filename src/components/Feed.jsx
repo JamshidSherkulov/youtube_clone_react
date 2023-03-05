b74cee66b66c983f0077497b34bcc75546b17cf3
JamshidSherkulov <jamshidsherkhulov@gmail.com>
2023-03-05 23:32:40 +0500

    data fetched displayed on home page

---
+++ src/components/Feed.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { SideBar, Videos } from "./allComponents";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const Feed = () => {
+
+  const[selectedCategory, setSelectedCategory] = useState('New')
+  const[videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then(data => setVideos(data.items))
+  }, [selectedCategory]);
+
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "95vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory} // sending props to sidebar
+        />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Jamshid Sherkulov
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflow: "auto", flex: 2, height: "90vh" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#f31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
