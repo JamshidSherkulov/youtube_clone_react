d42dbf726cdd6cf769380d1b149527c0791f2798
JamshidSherkulov <jamshidsherkhulov@gmail.com>
2023-03-09 17:31:00 +0500

    finished ready to deployement

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
-const ChannelCard = () => {
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({ channelDetail, marginTop }) => {
   return (
-    <div>ChannelCard</div>
-  )
-}
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: {sx: '356px', md: '320px'},
+        height: '326px',
+        margin: 'auto',
+        marginTop: marginTop
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: "180px",
+              width: "180px",
+              mb: 2,
+              border: "1px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6 ">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subcriberCount
+              ).toLocaleString()}
+              Subsicribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
+};
 
-export default ChannelCard
+// 1:27:00
+export default ChannelCard;
